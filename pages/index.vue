c9dab5d384ec64d899efa119df6f7b44221d91d3
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-09-15 11:14:35 -0300

    Create project. Add pages router

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hola from pages</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
