3b6b48d8c6f684852662ff597aff4e665e7bec33
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-09-16 22:54:22 -0300

    Handle error if not product is found

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -5,4 +5,8 @@
 <script setup>
 const id = useRoute().params.id
 const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`, { key: id })
+
+if (!product.value) {
+  throw createError({ statusCode: 404, statusMessage: "Product not found" })
+}
 </script>
