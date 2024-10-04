f57016228bf8634570ca82303850ebd6f7584993
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-10-04 20:42:36 -0300

    Add metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "NicoMarket",
+      meta: [
+        {
+          name: "description",
+          content: "Shop from Mercado Libre and Amazon. Special club group discounts",
+        },
+        { name: "author", content: "Nico Battaglia" },
+      ],
+    },
+  },
 })
