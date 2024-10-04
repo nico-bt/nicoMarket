7293884880c84fec532c8171f6d023de855fd688
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-10-04 20:44:56 -0300

    Add styles and type

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="card text-center">
     <img :src="`${product.image}`" class="thumb" />
-    <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+    <p class="font-bold text-gray-800 m-4 truncate">{{ product.title }}</p>
+    <p class="font-bold text-gray-600 m-4 truncate">$ {{ product.price.toLocaleString() }}</p>
+    <p class="font-bold text-gray-500 m-4 truncate">{{ product.source }}</p>
     <NuxtLink :to="`/products/${product.id}`">
       <p class="btn my-4">View Details</p>
     </NuxtLink>
   </div>
 </template>
 
-<script setup>
-const { product } = defineProps(["product"])
+<script setup lang="ts">
+import type { NormalizedProduct } from "@/server/api/products"
+
+const props = defineProps<{ product: NormalizedProduct }>()
 </script>
 
 <style scoped>
 .thumb {
-  height: 175px;
+  height: 160px;
   object-fit: contain;
   max-width: 70%;
   margin: 0 auto;
